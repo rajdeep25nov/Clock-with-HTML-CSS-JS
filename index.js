1b4a7da8142a52a6dad219fe493dcd95e92117dc
rajdeep25nov <74556611+rajdeep25nov@users.noreply.github.com>
2021-09-07 22:06:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    raj = new Date();
+    htime = raj.getHours();
+    mtime = raj.getMinutes();
+    stime = raj.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
